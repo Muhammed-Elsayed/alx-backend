39d29ae0fd313996f80d7fc28e45b843df82c4d0
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-07-09 03:47:14 +0300

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+""" BasicCache module"""
+
+
+class BasicCache(BaseCaching):
+    """ BaseCache defines:
+      -
+    """
+    """funct to append to the cache"""
+    def put(self, key, item):
+        if key is None or item is None:
+            pass
+
+        self.cache_data[key] = item
+
+    "funct to get a the value by the key"
+    def get(self, key):
+        if key is None or self.cache_data[key] is None:
+            return(None)
+        return(self.cache_data[key])
