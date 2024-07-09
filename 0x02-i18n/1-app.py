6375dd8c8595f6ce1ce1c8561ced494d4cb8e155
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-07-10 02:54:06 +0300

    1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""web app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """"config class for flask babel configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    """the home page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
